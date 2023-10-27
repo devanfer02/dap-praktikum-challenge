52096e5215836b079588414b04e917a204d20461
devanfer02 <ubdevanferrel04@student.ub.ac.id>
2023-10-27 22:02:26 +0700

    change: hero home

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,14 @@ function Footer() {
     <div className="container">
       <footer className="d-flex flex-wrap justify-content-between align-items-center py-1 my-2 border-top">
         <p className="col-md-4 mb-0 text-body-secondary">2023, Traveleen</p>
-        <a href="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+        <a href="index.html" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
           <img src="resources/icons/traveleen_logo.svg" alt="logo" />
         </a>
         <ul className="nav col-md-4 justify-content-end">
           <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Home</a></li>
           <li className="nav-item"><a href="https://www.instagram.com/dvnf20/" className="nav-link px-2 text-body-secondary">Instagram</a></li>
           <li className="nav-item"><a href="https://github.com/devanfer02" className="nav-link px-2 text-body-secondary">Github</a></li>
-          <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">About</a></li>
+          <li className="nav-item"><a href="about.html" className="nav-link px-2 text-body-secondary">About</a></li>
         </ul>
       </footer>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar({ activeLink }) {
     <nav className="navbar navbar-expand-lg bg-light">
       <div className="container-fluid">
         <div className="d-flex">
-          <img src="resources/icons/traveleen_logo.svg" alt="logo" width="50" className="img-fluid"/>
+          <img src="resources/icons/traveleen_logo.svg" alt="logo" width="50" className="img-fluid icon-nav" draggable='false'/>
           <h3 className="pt-2 logo-title"><b>Traveleen</b></h3>
         </div>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -15,17 +15,17 @@ function Navbar({ activeLink }) {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mb-2 mb-lg-0 ms-auto" style={{ paddingTop: '10px' }}>
-            <li className="nav-item">
+            <li className="nav-item nav-item-custom">
               <h5><a className={`nav-link ${isActive('Home')}`} aria-current="page" href="index.html">Home</a></h5>
             </li>
-            <li className="nav-item">
-              <h5><a className={`nav-link ${isActive('About')}`} href="about.html" >About</a></h5>
+            <li className="nav-item nav-item-custom">
+              <h5><a className={`nav-link ${isActive('About')}`} href="about.html">About</a></h5>
             </li>
-            <li className="nav-item">
-              <h5><a className="nav-link" href="signin.html">Login</a></h5>
+            <li className="nav-item nav-item-custom">
+              <h5><a className="nav-link auth-button" href="signin.html">&nbsp;Login&nbsp;</a></h5>
             </li>
-            <li className="nav-item">
-              <h5><a className="nav-link" href="signup.html" >Register</a></h5>
+            <li className="nav-item nav-item-custom">
+              <h5><a className="nav-link auth-button" href="signup.html">&nbsp;Register&nbsp;</a></h5>
             </li>
           </ul>
         </div>
