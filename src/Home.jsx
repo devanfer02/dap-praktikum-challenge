5028efe38c3e964a3ff48764879566e795360501
devanfer02 <ubdevanferrel04@student.ub.ac.id>
2023-10-29 19:07:44 +0700

    add: video

--- src/Home.jsx
+++ src/Home.jsx
@@ -15,16 +15,51 @@ function Home() {
     return 'Get Started Now'
   }
 
+  const controlVideo = () => {
+    const videoElement = document.getElementById('video-hero')
+    const containerDiv = document.getElementById('home-hero')
+    const imgElement = document.getElementById('icon-video')
+    
+    if (videoElement.getAttribute('autoplay') === 'true') {
+      containerDiv.style.backgroundImage = 'url(../resources/imgs/Home.jpg)'
+      videoElement.style.display = 'none'
+      videoElement.setAttribute('autoplay', false)
+      imgElement.src = 'resources/icons/jpeg/play24.png'
+      videoElement.pause()
+      videoElement.currentTime = 0
+    } else {
+      containerDiv.style.backgroundImage = 'none'
+      videoElement.style.display = 'block'
+      videoElement.setAttribute('autoplay', true)
+      imgElement.src = 'resources/icons/jpeg/stop1.png'
+      videoElement.play()
+    }
+
+  }
+
   return (
     <div>
       <Navbar activeLink={'Home'}/>
-      <div className="p-5 jumbotron text-white hero-container">
+      <div className="p-5 jumbotron text-white hero-container" id="home-hero">
+        <video muted playsInline id="video-hero" style={{ display: 'block' }}>
+          <source src="resources/videos/vidhero1.mp4" type="video/mp4"/>
+        </video>
         <div className="home-desc">
           <h1 className="hero-title"><b>Traveleen</b></h1>
           <hr/>  
           <p className="hero-content">At Traveleen, the journey begins the moment you arrive on our digital doorstep. We're more than just a website; we're your portal to the wonders of the world. Whether you're a seasoned traveler, an armchair explorer, or someone on the cusp of their first adventure, we're here to ignite your wanderlust, provide invaluable insights, and fuel your passion for exploring our remarkable planet. So, fasten your virtual seatbelt, get ready to roam, and let the adventure begin. Welcome to a world of boundless discovery and inspiration.
           </p>
-          <Button textContent={buttonTextDecider()} classList={'btn btn-modified'} onClick={homeHref}/>
+          <Button 
+            textContent={buttonTextDecider()} 
+            classList={'btn btn-modified'} 
+            onClick={homeHref}
+          />
+          <ButtonIcon 
+            iconSrc={'resources/icons/jpeg/play24.png'} 
+            classList={'btn btn-modified'} 
+            onClick={controlVideo}
+            style={{ marginLeft: '0.5em', alignItems: 'center' }}
+          />
         </div>
       </div>
       <Footer/>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,3 +3,11 @@ function Button({textContent, onClick, classList}) {
     <button type="button" className={classList} onClick={onClick}>{textContent}</button>
   )
 }
+
+function ButtonIcon({iconSrc, onClick, classList, style}) {
+  return (
+    <button type="button" className={classList} onClick={onClick} style={style}>
+      <img src={iconSrc} alt="icon" width={'20px'} height={'20px'} id="icon-video"/>
+    </button>
+  )
+}
