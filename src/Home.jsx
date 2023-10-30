c5aa237f39b07a3eae82725b1cf80fb5d4bb3510
devanfer02 <ubdevanferrel04@student.ub.ac.id>
2023-10-30 10:56:09 +0700

    add: animation text

--- src/Home.jsx
+++ src/Home.jsx
@@ -19,14 +19,17 @@ function Home() {
     const videoElement = document.getElementById('video-hero')
     const containerDiv = document.getElementById('home-hero')
     const imgElement = document.getElementById('icon-video')
+    const homeElemenet = document.getElementById('home-text')
 
     if (videoElement.style.display == 'block') {
+      homeElemenet.classList.toggle('dissapear-text')
       containerDiv.style.backgroundImage = 'url(./resources/imgs/Home.jpg)'
       videoElement.style.display = 'none'
       imgElement.src = 'resources/icons/jpeg/play24.png'
       videoElement.pause()
       videoElement.currentTime = 0
     } else {
+      homeElemenet.classList.toggle('dissapear-text')
       videoElement.play()
       containerDiv.style.backgroundImage = 'none'
       videoElement.style.display = 'block'
@@ -42,10 +45,13 @@ function Home() {
           <source src="resources/videos/vidhero.mp4" type="video/mp4"/>
         </video>
         <div className="home-desc">
-          <h1 className="hero-title"><b>Traveleen</b></h1>
-          <hr/>  
-          <p className="hero-content">At Traveleen, the journey begins the moment you arrive on our digital doorstep. We're more than just a website; we're your portal to the wonders of the world. Whether you're a seasoned traveler, an armchair explorer, or someone on the cusp of their first adventure, we're here to ignite your wanderlust, provide invaluable insights, and fuel your passion for exploring our remarkable planet. So, fasten your virtual seatbelt, get ready to roam, and let the adventure begin. Welcome to a world of boundless discovery and inspiration.
-          </p>
+          <div id="home-text">
+            <h1 className="hero-title"><b>Traveleen</b></h1>
+            <hr/>  
+            <p className="hero-content">
+              At Traveleen, the journey begins the moment you arrive on our digital doorstep. We're more than just a website; we're your portal to the wonders of the world. Whether you're a seasoned traveler, an armchair explorer, or someone on the cusp of their first adventure, we're here to ignite your wanderlust, provide invaluable insights, and fuel your passion for exploring our remarkable planet. So, fasten your virtual seatbelt, get ready to roam, and let the adventure begin. Welcome to a world of boundless discovery and inspiration.
+            </p>
+          </div>
           <Button 
             textContent={buttonTextDecider()} 
             classList={'btn btn-modified'} 
